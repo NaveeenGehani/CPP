8f9d31b213afc234160146891487f21ba0cc882e
Naveen Gehani <142261796+NaveeenGehani@users.noreply.github.com>
2025-07-05 11:20:17 +0000

    add janmansh-and-assignments problem

---
+++ logic-problems/easy/Janmansh-and-Assignments/assignments.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int x;
+	    cin >> x;
+	    if(10 - x >= 3){
+	       cout << "Yes" << endl;
+	    }else{
+	       cout << "No" << endl;
+	    }
+	}
+}

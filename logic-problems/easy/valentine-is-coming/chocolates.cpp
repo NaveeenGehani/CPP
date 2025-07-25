477f51d1ec57e14eab69d335238705cef4446cb6
Naveen Gehani <142261796+NaveeenGehani@users.noreply.github.com>
2025-07-25 19:28:57 +0000

    Add valentine-is-coming problem

---
+++ logic-problems/easy/valentine-is-coming/chocolates.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int x, y;
+	    cin >> x >> y;
+	    int chocolates = x / y;
+	    cout << chocolates << endl;
+	}
+	return 0;
+
+}

db0353e6f9ef0b44ac0eaa7d3d362a9b5dfd6494
Naveen Gehani <142261796+NaveeenGehani@users.noreply.github.com>
2025-07-30 20:19:18 +0000

    Made changes to chef-eren in the solution

--- logic-problems/easy/chef-eren/duration.cpp
+++ logic-problems/easy/chef-eren/duration.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main() {
-    // your code goes here
     int t;
     cin >> t;
     while (t--) {
@@ -33,7 +32,7 @@ int main() {
             // if n is odd, then there will be always more odd indexed episodes than even indexed episode.
             // a = even indexed episode length, b = odd indexed episode length
             // mulyiply a with number of even-indexed episodes and b with number of  odd-indexed episodes 
-            
+
             total_duration = (((n - 1) / 2) * a) + (((n + 1) / 2) * b);
         }
         cout << total_duration << endl;

ebc432da2f2822ec41d03e814ed27b4578f537ae
Naveen Gehani <142261796+NaveeenGehani@users.noreply.github.com>
2025-08-05 20:41:40 +0000

    Add too-many-items problem

---
+++ too-many-items/polyBags.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>  
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int min_polybags = ceil(n/10.0);
+	    cout << min_polybags << endl;
+	}
+	return 0;
+}
